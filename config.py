adacba7a0111111d12ce740aa44055361e9a1687
jian <jxx3451@mavs.uta.edu>
2025-12-15 16:09:39 -0600

    测试状态

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # 是否启用数据集裁剪模式（debug 加速加载）
-LIMITED_DATASIZE = True
+LIMITED_DATASIZE = False
 LOAD_DATASIZE = 20
 
 # 是否打印所有调试信息
-PRINT_EVERYTHING = True
+PRINT_EVERYTHING = False
 
 # 是否把计算结果移动到CPU节省内存
 CAL_RES_2_CPU = False
