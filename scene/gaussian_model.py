7ba8ce91356626a25ac293253f476f00cf342ed0
jian <jxx3451@mavs.uta.edu>
2026-01-03 16:47:30 -0600

    30k

--- scene/gaussian_model.py
+++ scene/gaussian_model.py
@@ -673,7 +673,7 @@ class GaussianModel:
         all_idx = torch.arange(N, device=device)
 
         new_block_indices = []
-        covered = torch.zeros(N, device=device, dtype=torch.bool)  # 记录是否被某个 block 覆盖
+        covered_mask = torch.zeros(N, device=device, dtype=torch.bool)  # 记录是否被某个 block 覆盖
 
         # ---------- 遍历每个 block ----------
         for i, (mn, mx) in enumerate(self.block_bounds):
@@ -689,10 +689,11 @@ class GaussianModel:
 
             idx = all_idx[mask]
             new_block_indices.append(idx)
-            covered[idx] = True  # 标记被覆盖
+            covered_mask[idx] = True  # 标记被覆盖
 
             # debug 用
             # print(f"[repartition] Block {i:2d}: {idx.numel():7d} points")
-        uncovered_idx = all_idx[~covered]
+        uncovered_idx = all_idx[~covered_mask]
+        self.prune_points(~covered_mask)
         self.block_indices = new_block_indices
-        print(f"[repartition] Uncovered points: {uncovered_idx.numel()}")
+        print(f"[repartition] Uncovered points ({uncovered_idx.numel()}): {uncovered_idx.tolist()}")
