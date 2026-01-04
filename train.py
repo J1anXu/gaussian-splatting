bee27cb6ac0d28547c0848fed537361a1c4a455e
jian <jxx3451@mavs.uta.edu>
2026-01-03 22:42:49 -0600

    11

--- train.py
+++ train.py
@@ -103,7 +103,7 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
         available_indices = torch.nonzero(available_mask, as_tuple=True)[0]
         
         if not gaussians.partitioned:
-            if gaussians._xyz.shape[0] > 50_000:
+            if gaussians._xyz.shape[0] > 300_000:
                 gaussians.partition() 
                 gaussians.visualize_blocks(save_path = f"debug/{BRANCH}_bbox")
                 gaussians.partitioned = True
