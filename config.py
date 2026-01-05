7f209f7d653044fde0cd99490a9177ed4a85ff71
jian <jxx3451@mavs.uta.edu>
2026-01-04 21:04:45 -0600

    111

--- config.py
+++ config.py
@@ -9,3 +9,6 @@ CAL_RES_2_CPU = False  #把计算结果移动到CPU节省内存 (能节省内存
 
 SAVE_BLOCK_IMG = True  # 是否保存 block 渲染结果
 DRAW_BOX = False # 是否在 block 渲染结果上绘制点云和block的边界框
+
+
+SAVE_RGB_LAYERS = True  # 是否保存合并前的各个RGB图层
