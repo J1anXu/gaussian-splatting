4fd42a97857eb03d5615b13c920ac39d4890a432
jian <jxx3451@mavs.uta.edu>
2026-01-05 15:43:18 -0600

    fix

--- train.py
+++ train.py
@@ -150,7 +150,7 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
             alpha_mask = viewpoint_cam.alpha_mask.cuda()
             image *= alpha_mask
         
-        debug_image_name = "_DSC8681.JPG"
+        debug_image_name = "_DSC8680.JPG"
         if viewpoint_cam.image_name == debug_image_name:
             img_path_in_debug = os.path.join("debug", BRANCH, debug_image_name, f"iter_{iteration}")
             os.makedirs(img_path_in_debug, exist_ok=True)
