d572a55116aa40e5107b053c11998b0988383220
jian <jxx3451@mavs.uta.edu>
2026-01-04 21:39:37 -0600

    good

--- config.py
+++ config.py
@@ -8,8 +8,9 @@ DEBUG_MODE = True
 CAL_RES_2_CPU = False  #把计算结果移动到CPU节省内存 (能节省内存但是移动非常慢)
 
 SAVE_BLOCK_IMG = True  # 是否保存 block 渲染结果
-DRAW_BOX = False # 是否在 block 渲染结果上绘制点云和block的边界框
+DRAW_BOX = True # 是否在 block 渲染结果上绘制点云和block的边界框
 
 
 SAVE_RGB_LAYERS = True  # 是否保存合并前的各个RGB图层
 SAVE_LAYERS_CONTRIBUTION = True  # 是否保存每个block对各个图层的贡献
+SAVE_DEPTH_LIST = True  # 是否保存各个block的深度图
