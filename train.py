58476ba0722788c81cee501a0f9ab4c9a8df17eb
jian <jxx3451@mavs.uta.edu>
2026-01-07 15:19:47 -0600

    partition at 15000

--- train.py
+++ train.py
@@ -109,7 +109,7 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
         available_indices = torch.nonzero(available_mask, as_tuple=True)[0]
         
         if not gaussians.partitioned:
-            if gaussians._xyz.shape[0] > 300_000:
+            if iteration > 15000:
                 gaussians.partition() 
                 gaussians.visualize_blocks(save_path = f"debug/{BRANCH}_bbox")
                 gaussians.partitioned = True
