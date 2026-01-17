a14080807f398493912f5094e060a3f4f8caaa29
jian <jxx3451@mavs.uta.edu>
2026-01-17 15:01:02 -0600

    branch: BRANCH

--- metrics_p.py
+++ metrics_p.py
@@ -83,7 +83,8 @@ def evaluate(model_paths):
 
                 full_dict[scene_dir][method].update({"SSIM": torch.tensor(ssims).mean().item(),
                                                         "PSNR": torch.tensor(psnrs).mean().item(),
-                                                        "LPIPS": torch.tensor(lpipss).mean().item()})
+                                                        "LPIPS": torch.tensor(lpipss).mean().item(),
+                                                        "branch": BRANCH})
                 per_view_dict[scene_dir][method].update({"SSIM": {name: ssim for ssim, name in zip(torch.tensor(ssims).tolist(), image_names)},
                                                             "PSNR": {name: psnr for psnr, name in zip(torch.tensor(psnrs).tolist(), image_names)},
                                                             "LPIPS": {name: lp for lp, name in zip(torch.tensor(lpipss).tolist(), image_names)}})
