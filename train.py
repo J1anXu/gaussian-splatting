9fc206cb2e4db8e72ed32bdbaabf30f7e17ba827
jian <jxx3451@mavs.uta.edu>
2026-01-19 17:43:36 -0600

    fix run_all;

--- train.py
+++ train.py
@@ -122,8 +122,9 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
         
         # 视锥剔除
         for model in model_list:
-            # visible_mask = frustum_culling(model._xyz, viewpoint_cam.full_proj_transform)
-            visible_mask = torch.ones(model._xyz.shape[0], dtype=torch.bool, device=model._xyz.device)
+            visible_mask = frustum_culling(model._xyz, viewpoint_cam.full_proj_transform)
+            if visible_mask.sum() == 0:
+                visible_mask = torch.ones(model._xyz.shape[0], dtype=torch.bool, device=model._xyz.device)
             model.visible_idx = torch.nonzero(visible_mask, as_tuple=True)[0]
         
         rendered_list, depth_list, alpha_list = [], [], []
