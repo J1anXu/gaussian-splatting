e518ee83bf3244d4b2ca9d1c911b7f230c9611a7
jian <jxx3451@mavs.uta.edu>
2026-01-20 11:29:43 -0600

    update

--- config.py
+++ config.py
@@ -15,5 +15,5 @@ SAVE_LAYERS_CONTRIBUTION = True  # 是否保存每个block对各个图层的贡�
 SAVE_DEPTH_LIST = True  # 是否保存各个block的深度图
 
 
-PARTITIONING_ENABLED = True  # 是否启用分块处理
+PARTITIONING_ENABLED = False  # 是否启用分块处理
 FRUSTUM_CULLING_ENABLED = False  # 是否启用视锥剔除
