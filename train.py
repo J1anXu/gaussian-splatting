03ec41861025659a2a16acd1eda72ec3d5c01e4d
jian <jxx3451@mavs.uta.edu>
2026-01-31 16:29:03 -0600

    partgs

--- train.py
+++ train.py
@@ -340,7 +340,7 @@ if __name__ == "__main__":
     if WANDB and not DEBUG_MODE:
         wandb.login()
         run = wandb.init(
-            project = "fcsp", 
+            project = "partgs", 
             name = f"{SCENE_NAME}_{BRANCH}_{time.strftime('%m%d%H%M')}", 
             job_type = "train", 
             group = SCENE_NAME,
