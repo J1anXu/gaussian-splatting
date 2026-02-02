706f5bc9af236a5365db6532a4dfa1e89c5ab395
Jian <jxx3451@mavs.uta.edu>
2026-02-02 12:13:33 -0600

    move dataset to cpu

--- utils/camera_utils.py
+++ utils/camera_utils.py
@@ -22,7 +22,8 @@ from tqdm import tqdm
 WARNED = False
 
 def loadCam(args, id, cam_info, resolution_scale, is_nerf_synthetic, is_test_dataset):
-    image = Image.open(cam_info.image_path)
+    with Image.open(cam_info.image_path) as img:
+        image = img.copy()  
 
     if cam_info.depth_path != "":
         try:
@@ -67,7 +68,7 @@ def loadCam(args, id, cam_info, resolution_scale, is_nerf_synthetic, is_test_dat
     return Camera(resolution, colmap_id=cam_info.uid, R=cam_info.R, T=cam_info.T, 
                   FoVx=cam_info.FovX, FoVy=cam_info.FovY, depth_params=cam_info.depth_params,
                   image=image, invdepthmap=invdepthmap,
-                  image_name=cam_info.image_name, uid=id, data_device=args.data_device,
+                  image_name=cam_info.image_name, uid=id, data_device="cpu", # jian
                   train_test_exp=args.train_test_exp, is_test_dataset=is_test_dataset, is_test_view=cam_info.is_test)
 
 def cameraList_from_camInfos(cam_infos, resolution_scale, args, is_nerf_synthetic, is_test_dataset):
