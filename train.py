4405c072ec0b2df4c60167ffa2758f5710e0c059
jian <jxx3451@mavs.uta.edu>
2026-02-03 19:02:01 -0600

    parser.add_argument('--data_device', type=str, default=cpu)

--- train.py
+++ train.py
@@ -476,6 +476,8 @@ if __name__ == "__main__":
     parser.add_argument("--checkpoint_iterations", nargs="+", type=int, default=[7_000, 15_000, 30_000])
     parser.add_argument("--start_checkpoint", type=str, default = None)
     parser.add_argument('--git_branch', type=str, default=None)
+    parser.add_argument('--data_device', type=str, default="cpu")
+
     args = parser.parse_args(sys.argv[1:])
     args.save_iterations.append(args.iterations)
     
