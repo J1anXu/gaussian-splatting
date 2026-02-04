e77d3fbda77c5445e541a830c48568cf6f01992a
jian <jxx3451@mavs.uta.edu>
2026-02-03 19:50:43 -0600

    merge

--- train.py
+++ train.py
@@ -71,14 +71,6 @@ def training_phase_1(dataset, opt, pipe, checkpoint, debug_from):
     initial_gaussians = GaussianModel(dataset.sh_degree, opt.optimizer_type)
     scene = Scene(dataset, initial_gaussians, on_cpu=True)
     
-    
-    # TODO
-    scene.gaussians.load_ply("/home/jian/gaussian-splatting-2/output/mip360/bicycle/point_cloud/baseline/iteration_30000/point_cloud.ply")
-    progress_bar = tqdm(range(first_iter, opt.iterations), desc="Training progress")
-
-    return scene, 1000, 0, 0, progress_bar, None
-    
-    
     initial_gaussians.training_setup(opt)
     
     if checkpoint:
