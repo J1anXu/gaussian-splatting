6c0876f12b62fd9691d9403b0d1499ea91b5765d
jian <jxx3451@mavs.uta.edu>
2026-02-21 00:55:54 -0600

    fix

--- run_all.sh
+++ run_all.sh
@@ -79,7 +79,7 @@ run_pipeline() {
   ####################
   # 2. RENDER_P
   ####################
-  python render.py \
+  python render_p.py \
     -m "$model_path" \
     --git_branch "$GIT_BRANCH" \
     --skip_train \
@@ -88,7 +88,7 @@ run_pipeline() {
   ####################
   # 3. METRICS_P
   ####################
-  python metrics.py \
+  python metrics_p.py \
     -m "$model_path" \
     --git_branch "$GIT_BRANCH" \
     > "$log_dir/metricsp.log" 2>&1
