10eecf34b64e7881873b0ffdb3adb140f4648385
jian <jxx3451@mavs.uta.edu>
2026-02-21 19:37:38 -0600

    fix

--- run_all.sh
+++ run_all.sh
@@ -69,12 +69,12 @@ run_pipeline() {
   # ####################
   # # 1. TRAIN
   # ####################
-  python train.py \
-    -s "$data_path" \
-    --model_path "$model_path" \
-    --git_branch "$GIT_BRANCH" \
-    --eval \
-    > "$log_dir/train.log" 2>&1
+  # python train.py \
+  #   -s "$data_path" \
+  #   --model_path "$model_path" \
+  #   --git_branch "$GIT_BRANCH" \
+  #   --eval \
+  #   > "$log_dir/train.log" 2>&1
 
   ####################
   # 2. RENDER_P
