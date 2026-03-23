c4fa992b29d79959cbe89732af3f01abd66c5d79
Jian <jxx3451@mavs.uta.edu>
2026-03-23 14:27:07 -0500

    no draw

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ DATASIZE_LIMIT = 5
 
 CAL_RES_2_CPU = False  #把计算结果移动到CPU节省内存 (能节省内存但是移动非常慢)
 
-SAVE_BLOCK_IMG = True  # 是否保存 block 渲染结果
+SAVE_BLOCK_IMG = False  # 是否保存 block 渲染结果
 DRAW_BOX = False # 是否在 block 渲染结果上绘制点云和block的边界框
 
 
-SAVE_RGB_LAYERS = True  # 是否保存合并前的各个RGB图层
-SAVE_LAYERS_CONTRIBUTION = True  # 是否保存每个block对各个图层的贡献
-SAVE_DEPTH_LIST = True  # 是否保存各个block的深度图
+SAVE_RGB_LAYERS = False  # 是否保存合并前的各个RGB图层
+SAVE_LAYERS_CONTRIBUTION = False  # 是否保存每个block对各个图层的贡献
+SAVE_DEPTH_LIST = False  # 是否保存各个block的深度图
 
 
 PARTITIONING_ENABLED = True  # 是否启用分块处理
