12a029ae4d9ceff5b0ec4018f85b258efbbf4cf3
Jian <jxx3451@mavs.uta.edu>
2026-03-25 13:02:25 -0500

    CapGS

--- runall_4090.sh
+++ runall_4090.sh
@@ -24,7 +24,7 @@ set -o pipefail
 GPUS=(1)
 NUM_GPUS=${#GPUS[@]}
 DATA_BASE=/data/jian/data
-OUT_BASE=/data/jian/output/ours
+OUT_BASE=/data/jian/output/CapGS
 LOG_ROOT=debug
 
 REPO_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
--- runall_ada0.sh
+++ runall_ada0.sh
@@ -24,7 +24,7 @@ set -o pipefail
 GPUS=(0)
 NUM_GPUS=${#GPUS[@]}
 DATA_BASE=/data2/jian/data
-OUT_BASE=/data2/jian/output
+OUT_BASE=/data2/jian/output/CapGS
 LOG_ROOT=debug
 
 REPO_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
