383bd0e8d887bb34d236756358123c0f5aa2c357
Jian <jxx3451@mavs.uta.edu>
2026-03-25 12:54:49 -0500

    40K for inner

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ FRUSTUM_CULLING_CACHE_ENABLED = True  # 是否缓存视锥剔除结果（densify
 INDOOR_SCENES = {"room", "counter", "kitchen", "bonsai", "drjohnson", "playroom"}
 OUTDOOR_SCENES = {"bicycle", "flowers", "garden", "stump", "treehill", "train", "truck"}
 
-SPLIT_SIZE_INDOOR = 200000
+SPLIT_SIZE_INDOOR = 400000
 SPLIT_SIZE_OUTDOOR = 800000
 
 SPLIT_SIZE = SPLIT_SIZE_OUTDOOR  # default, overridden at runtime by scene type
